db99968f0387aa1657345f3f29fa2c5a317c9049
lwsporcello <79470244+lwsporcello@users.noreply.github.com>
2022-09-15 16:15:17 -0400

    add example for use with existing iam roles

--- examples/use_existing_iam_roles/main.tf
+++ examples/use_existing_iam_roles/main.tf
@@ -1,7 +1,14 @@
 provider "lacework" {}
 
 module "aws_eks_audit_log" {
-  source             = "../.."
-  cloudwatch_regions = ["us-west-1"]
-  cluster_names      = ["example_cluster"]
+  source                              = "../.."
+  cloudwatch_regions                  = ["us-west-1"]
+  cluster_names                       = ["example_cluster"]
+  use_existing_cross_account_iam_role = true
+  iam_role_arn                        = "arn:aws:iam::123456789012:role/my-ca-role"
+  iam_role_external_id                = "abc123"
+  use_existing_cloudwatch_iam_role    = true
+  cloudwatch_iam_role_arn             = "arn:aws:iam::123456789012:role/my-cw-role"
+  use_existing_firehose_iam_role      = true
+  firehose_iam_role_arn               = "arn:aws:iam::123456789012:role/my-fh-role"
 }
