dc48ab1787a58d766224bb7c9b6ab1a9d1e35445
Darren <75614232+dmurray-lacework@users.noreply.github.com>
2022-09-21 14:52:03 +0100

    test: update examples with new output name (#15)
    * test: update examples with new output name

--- examples/encryption_enabled_existing_kms_key/main.tf
+++ examples/encryption_enabled_existing_kms_key/main.tf
@@ -8,7 +8,7 @@ module "aws_eks_audit_log" {
   source                              = "../.."
   cloudwatch_regions                  = ["us-west-2"]
   cluster_names                       = ["my-tf-cluster"]
-  bucket_sse_key_arn                  = "arn:aws:kms:us-west-2:123456789012:key/mrk-1234567890abcdefghijklmnopqrstuv"
-  kinesis_firehose_encryption_key_arn = "arn:aws:kms:us-west-2:123456789012:key/mrk-1234567890abcdefghijklmnopqrstuv"
-  sns_topic_encryption_key_arn        = "arn:aws:kms:us-west-2:123456789012:key/mrk-1234567890abcdefghijklmnopqrstuv"
+  bucket_key_arn                      = "arn:aws:kms:us-west-2:123456789012:key/mrk-1234567890abcdefghijklmnopqrstuv"
+  kinesis_firehose_key_arn            = "arn:aws:kms:us-west-2:123456789012:key/mrk-1234567890abcdefghijklmnopqrstuv"
+  sns_topic_key_arn                   = "arn:aws:kms:us-west-2:123456789012:key/mrk-1234567890abcdefghijklmnopqrstuv"
 }
