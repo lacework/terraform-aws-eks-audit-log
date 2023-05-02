790d70d3c04f9e5e5b63e46d188780a684857ba1
David McTavish <david.mctavish@lacework.net>
2023-05-02 12:40:12 +0100

    fix: update readme

--- examples/use_existing_bucket/main.tf
+++ examples/use_existing_bucket/main.tf
@@ -5,5 +5,5 @@ module "aws_eks_audit_log" {
   cloudwatch_regions  = ["us-west-2"]
   cluster_names       = ["example_cluster"]
   use_existing_bucket = true
-  bucket_arn          = "arn:aws:s3:::dev5-cloud-account-registrar-lacework"
+  bucket_arn          = "arn:aws:s3:::lacework-eks-bucket-8805c0bf"
 }
